ee9681fd606635e8c9342d12cc86cbf5d491e8ca
Elián Bargaz <122175106+Elibar19@users.noreply.github.com>
2023-10-17 22:59:04 -0300

    1.1B

--- Program.cs
+++ Program.cs
@@ -1,53 +1,61 @@
 ﻿using System;
-using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        Estacionamiento estacionamiento = new Estacionamiento();
+        List<sistemaEspacial> sistemas = new List<sistemaEspacial>();
+        Random random = new Random();
 
         while (true)
         {
             Console.WriteLine("Menú:");
-            Console.WriteLine("1. Listar todos los vehículos");
-            Console.WriteLine("2. Agregar un nuevo vehículo");
-            Console.WriteLine("3. Remover un vehículo por matrícula");
-            Console.WriteLine("4. Remover un vehículo por DNI del dueño");
-            Console.WriteLine("5. Remover una cantidad aleatoria de vehículos");
-            Console.WriteLine("6. Optimizar el espacio");
-            Console.WriteLine("7. Salir");
+            Console.WriteLine("1. Simular un sistema y generar asteroides");
+            Console.WriteLine("2. Procesar asteroides del sistema");
+            Console.WriteLine("3. Salir del sistema y generar un reporte");
+            Console.WriteLine("4. Salir del programa y generar un reporte general");
+            Console.WriteLine("5. Salir");
 
             string option = Console.ReadLine();
 
             switch (option)
             {
                 case "1":
-                    estacionamiento.listarTodos();
+                    sistemas.Add(sistemaEspacial.generarSistemaRandom(random));
+                    Console.WriteLine("Sistema generado con asteroides aleatorios.");
                     break;
                 case "2":
-                    estacionamiento.añadirVehiculoRandom();
+                    if (sistemas.Count > 0)
+                    {
+                        sistemaEspacial sistemaActual = sistemas.Last();
+                        sistemaActual.procesarAsteroide();
+                        Console.WriteLine("Asteroides procesados en el sistema actual.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No hay sistemas generados para procesar asteroides.");
+                    }
                     break;
                 case "3":
-                    Console.Write("Ingrese la matrícula del vehículo a remover: ");
-                    string patente = Console.ReadLine();
-                    estacionamiento.eliminarPorPatente(patente);
+                    if (sistemas.Count > 0)
+                    {
+                        sistemaEspacial sistemaActual = sistemas.Last();
+                        sistemaActual.generarReporte();
+                        sistemas.Remove(sistemaActual);
+                        Console.WriteLine("Reporte del sistema generado.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No hay sistemas generados para generar un reporte.");
+                    }
                     break;
                 case "4":
-                    Console.Write("Ingrese el DNI del dueño del vehículo a remover: ");
-                    string dni = Console.ReadLine();
-                    estacionamiento.eliminarPorDNI(dni);
-                    break;
+                    generarReporteGeneral(sistemas);
+                    return;
                 case "5":
-                    Console.Write("Ingrese la cantidad de vehículos a remover: ");
-                    int cantidad = int.Parse(Console.ReadLine());
-                    estacionamiento.eliminarRandom(cantidad);
-                    break;
-                case "6":
-                    estacionamiento.optimizar();
-                    break;
-                case "7":
                     return;
                 default:
                     Console.WriteLine("Opción no válida. Intente nuevamente.");
@@ -55,149 +63,123 @@ class Program
             }
         }
     }
-}
 
-class Vehiculo
-{
-    public string Modelo { get; set; }
-    public string DueñoDNI { get; set; }
-    public string Patente { get; set; }
-    public double Largo { get; set; }
-    public double Ancho { get; set; }
-}
-
-class Estacionamiento
-{
-    private List<Vehiculo> regular = new List<Vehiculo>();
-    private List<Vehiculo> quantum = new List<Vehiculo>();
-    private Random random = new Random();
-
-    public void listarTodos()
+    static void generarReporteGeneral(List<sistemaEspacial> sistemas)
     {
-        Console.WriteLine("Vehículos en el estacionamiento regular:");
-        foreach (var vehiculo in regular)
+        int totalHierro = 0;
+        int totalOro = 0;
+        int totalPlatino = 0;
+        int totalMiscelaneos = 0;
+        int totalCarga = 0;
+
+        foreach (var sistema in sistemas)
         {
-            Console.WriteLine($"Modelo: {vehiculo.Modelo}, Matrícula: {vehiculo.Patente}, Dueño: {vehiculo.DueñoDNI}");
+            totalHierro += sistema.cantidadHierro;
+            totalOro += sistema.cantidadOro;
+            totalPlatino += sistema.cantidadPlatino;
+            totalMiscelaneos += sistema.cantidadMiscelaneo;
+            totalCarga += sistema.cantidadTotal;
         }
 
-        Console.WriteLine("Vehículos en el estacionamiento cuántico:");
-        foreach (var vehiculo in quantum)
+        Console.WriteLine("Reporte General:");
+        Console.WriteLine($"Total de sistemas procesados: {sistemas.Count}");
+        Console.WriteLine($"Total de carga procesada: {totalCarga} KG");
+        Console.WriteLine($"Total de hierro: {totalHierro} KG");
+        Console.WriteLine($"Total de oro: {totalOro} KG");
+        Console.WriteLine($"Total de platino: {totalPlatino} KG");
+        Console.WriteLine($"Total de metales misceláneos: {totalMiscelaneos} KG");
+    }
+}
+
+class sistemaEspacial
+{
+    public string codigoSistema { get; }
+    public List<Asteroide> Asteroides { get; }
+    public int cantidadHierro { get; private set; }
+    public int cantidadOro { get; private set; }
+    public int cantidadPlatino { get; private set; }
+    public int cantidadMiscelaneo { get; private set; }
+    public int cantidadTotal => cantidadHierro + cantidadOro + cantidadPlatino + cantidadMiscelaneo;
+
+    public sistemaEspacial(string codigo, List<Asteroide> asteroides)
+    {
+        codigoSistema = codigo;
+        Asteroides = asteroides;
+    }
+
+    public void procesarAsteroide()
+    {
+        foreach (var asteroide in Asteroides)
         {
-            Console.WriteLine($"Modelo: {vehiculo.Modelo}, Matrícula: {vehiculo.Patente}, Dueño: {vehiculo.DueñoDNI}");
+            cantidadHierro += asteroide.Hierro;
+            cantidadOro += asteroide.Oro;
+            cantidadPlatino += asteroide.Platino;
+            cantidadMiscelaneo += asteroide.Miscelaneo;
         }
     }
 
-    public void añadirVehiculoRandom()
+    public void generarReporte()
     {
-        string[] modelos = { "Modelo1", "Modelo2", "Modelo3" };
-        string[] dueños = { "DNI1", "DNI2", "DNI3" };
-        double randomLargo = random.NextDouble() * 5 + 1;
-        double randomAncho = random.NextDouble() * 0.5 + 1.5;
+        Console.WriteLine($"EN EL SISTEMA [{codigoSistema}] SE MINARON [{Asteroides.Count}] ASTEROIDES");
+        Console.WriteLine($"{cantidadHierro} KG de hierro");
+        Console.WriteLine($"{cantidadOro} KG de oro");
+        Console.WriteLine($"{cantidadPlatino} KG de platino");
+        Console.WriteLine($"{cantidadMiscelaneo} KG de metales misceláneos");
+        Console.WriteLine($"Por un total de {cantidadTotal} KG de carga.");
+    }
 
-        Vehiculo vehicle = new Vehiculo
+    public static sistemaEspacial generarSistemaRandom(Random random)
+    {
+        string codigoSistema = Guid.NewGuid().ToString().Substring(0, 8);
+        int numAsteroides = random.Next(5, 11); // Generar de 5 a 10 asteroides
+        
+        List<Asteroide> asteroides = new List<Asteroide>();
+        for (int i = 0; i < numAsteroides; i++)
         {
-            Modelo = modelos[random.Next(modelos.Length)],
-            DueñoDNI = dueños[random.Next(dueños.Length)],
-            Patente = Guid.NewGuid().ToString().Substring(0, 8),
-            Largo = randomLargo,
-            Ancho = randomAncho
+            Asteroide asteroide = Asteroide.generarAsteroideRandom(random);
+            asteroides.Add(asteroide);
+        }
+
+        return new sistemaEspacial(codigoSistema, asteroides);
+    }
+}
+
+class Asteroide
+{
+    public int Hierro { get; }
+    public int Oro { get; }
+    public int Platino { get; }
+    public int Miscelaneo { get; }
+
+    public Asteroide(int hierro, int oro, int platino, int miscelaneo)
+    {
+        Hierro = hierro;
+        Oro = oro;
+        Platino = platino;
+        Miscelaneo = miscelaneo;
+    }
+
+    public static Asteroide generarAsteroideRandom(Random random)
+    {
+        int tamaño = random.Next(4); // tamaños
+        int hierro = tamaño switch
+        {
+            0 => random.Next(251, 751),
+            1 => random.Next(501, 1501),
+            2 => random.Next(1251, 3751),
+            _ => random.Next(2501, 7501),
         };
-
-        if (random.Next(2) == 0)
+        int peso = tamaño switch
         {
-            regular.Add(vehicle);
-        }
-        else
-        {
-            quantum.Add(vehicle);
-        }
+            0 => 1000,
+            1 => 2000,
+            2 => 5000,
+            _ => 10000,
+        };
+        int oro = random.Next(0, peso - hierro + 1);
+        int platino = random.Next(0, peso - hierro - oro + 1);
+        int miscelaneo = peso - hierro - oro - platino;
 
-        Console.WriteLine("Vehículo agregado con éxito.");
-    }
-
-    public void eliminarPorPatente(string vehiculoPatente)
-    {
-        Vehiculo vehicleToRemove = regular.Find(v => v.Patente == vehiculoPatente);
-        if (vehicleToRemove != null)
-        {
-            regular.Remove(vehicleToRemove);
-            Console.WriteLine($"Vehículo con matrícula {vehiculoPatente} removido del estacionamiento regular.");
-            return;
-        }
-
-        vehicleToRemove = quantum.Find(v => v.Patente == vehiculoPatente);
-        if (vehicleToRemove != null)
-        {
-            quantum.Remove(vehicleToRemove);
-            Console.WriteLine($"Vehículo con matrícula {vehiculoPatente} removido del estacionamiento cuántico.");
-        }
-        else
-        {
-            Console.WriteLine($"Vehículo con matrícula {vehiculoPatente} no encontrado.");
-        }
-    }
-
-    public void eliminarPorDNI(string dni)
-    {
-        Vehiculo vehicleToRemove = regular.Find(v => v.DueñoDNI == dni);
-        if (vehicleToRemove != null)
-        {
-            regular.Remove(vehicleToRemove);
-            Console.WriteLine($"Vehículo del dueño con DNI {dni} removido del estacionamiento regular.");
-            return;
-        }
-
-        vehicleToRemove = quantum.Find(v => v.DueñoDNI == dni);
-        if (vehicleToRemove != null)
-        {
-            quantum.Remove(vehicleToRemove);
-            Console.WriteLine($"Vehículo del dueño con DNI {dni} removido del estacionamiento cuántico.");
-        }
-        else
-        {
-            Console.WriteLine($"Vehículo del dueño con DNI {dni} no encontrado.");
-        }
-    }
-
-    public void eliminarRandom(int cantidad)
-    {
-        for (int i = 0; i < cantidad; i++)
-        {
-            if (regular.Count > 0 && random.Next(2) == 0)
-            {
-                regular.RemoveAt(random.Next(regular.Count));
-            }
-            else if (quantum.Count > 0)
-            {
-                quantum.RemoveAt(random.Next(quantum.Count));
-            }
-        }
-
-        Console.WriteLine($"{cantidad} vehículos removidos aleatoriamente.");
-    }
-
-    public void optimizar()
-    {
-        List<Vehiculo> noOptimizados = new List<Vehiculo>();
-        noOptimizados.AddRange(regular);
-        noOptimizados.AddRange(quantum);
-
-        regular.Clear();
-        quantum.Clear();
-
-        foreach (var vehiculo in noOptimizados)
-        {
-            if (vehiculo.Largo <= 5 && vehiculo.Ancho <= 2)
-            {
-                regular.Add(vehiculo);
-            }
-            else
-            {
-                quantum.Add(vehiculo);
-            }
-        }
-
-        Console.WriteLine("Espacio optimizado con éxito.");
+        return new Asteroide(hierro, oro, platino, miscelaneo);
     }
 }
-
